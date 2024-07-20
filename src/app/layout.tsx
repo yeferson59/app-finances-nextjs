025e139907abad71505fa733d5bcec30df8cd5ad
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-07-19 19:22:45 -0500

    quit of styles and header general

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
-import Header from "@/components/header";
 import { APP } from "@/lib/consts";
 
 const fontSans = FontSans({ 
@@ -35,8 +34,7 @@ export default function RootLayout({
         enableSystem
         disableTransitionOnChange
         >
-          <Header />
-          <main className="flex flex-col items-center justify-between px-10 py-4 max-w-5xl mx-auto">
+          <main>
             {children}
           </main>
         </ThemeProvider>
