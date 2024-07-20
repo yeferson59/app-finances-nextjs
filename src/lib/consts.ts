c89da424e2109add47ce8b95289b91142a17c126
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-07-19 19:21:43 -0500

    add links

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,7 +2,27 @@ export const NAVIGATION = [
     {
         id: 1,
         name: "Descripción",
-        link: "#description"
+        link: "/#description"
+    },
+    {
+        id: 2,
+        name: "Mercados",
+        link: "/#market"
+    },
+    {
+        id: 3,
+        name: "Cartera",
+        link: "/#portfolio"
+    },
+    {
+        id: 4,
+        name: "Noticias",
+        link: "/#news"
+    },
+    {
+        id: 5,
+        name: "Ayuda",
+        link: "#help"
     }
 ]
 
