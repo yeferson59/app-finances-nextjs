16bfdbf96cae97348e6db9477980505974397b63
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-07-19 22:59:02 -0500

    improve view mobile for login

--- src/components/navPhone.tsx
+++ src/components/navPhone.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 import { ModeToggle } from "@/components/button-theme"
 import Link from "next/link";
 import { Menu } from "lucide-react"
-import { NAVIGATION } from "@/lib/consts";
+import { NAVIGATION, NAVIGATION_AUTH } from "@/lib/consts";
 
 import {
   DropdownMenu,
@@ -46,29 +46,26 @@ export function DropdownMenuRadioGroupDemo() {
               </li>
             ))
           }
+          <DropdownMenuSeparator />
         </ul>
         </DropdownMenuRadioGroup>
-        <div className="flex md:flex-row items-center gap-2 justify-between">
+        <section className="flex flex-col items-center justify-center gap-2 mt-2">
+          {
+            pathname === '/' && (
+              NAVIGATION_AUTH.map( auth => (
+                <Link
+                key={auth.id}
+                href={auth.link}
+                >
+                  <Button className="text-pretty">
+                    {auth.name}
+                  </Button>
+                </Link>
+              ))
+            )
+          }
           <ModeToggle/>
-          {
-            pathname === '/' && (
-              <Link href='/login/sign-in'>
-                <Button className="text-pretty">
-                  Iniciar sesión
-                </Button>
-              </Link>
-            )
-          }
-          {
-            pathname === '/' && (
-              <Link href='/login/sign-up'>
-                <Button className="text-pretty">
-                  Registrarse
-                </Button>
-              </Link>
-            )
-          }
-        </div>
+        </section>
       </DropdownMenuContent>
     </DropdownMenu>
   );
